91d9dcb806e98b1bdc6bd7c34baf3d5b5be53db8
Suehring <ssuehrin@uwsp.edu>
2025-09-24 08:58:28 -0500

    09-24-2025

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+print "<!DOCTYPE html>";
+print "<html lang=\"en\">";
+print "<head>";
+print "<title>Minimal Page</title>";
+print "</head>";
+print "<body>";
+print "<h1>Hello World</h1>";
+print "</body>";
+print "</html>";
